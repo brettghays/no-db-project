559ade7bd4e6fb601b0ea1fd7a27bb34d23228c2
brett.g.hays <brett.g.hays.coding@gmail.com>
2018-01-01 18:37:09 -0700

    roster controller completed (i think)

---
+++ server/controllers/roster_controller.js
@@ -0,0 +1,39 @@
+let roster = [];
+let id = 0;
+
+module.export = {
+    create: (req,res) => {
+        const {firstName, lastName, jerseyNumber, height, position} = req.body;
+        roster.push({id, firstName, lastName, jerseyNumber, height, position});
+        id++;
+        res.status(200).send(roster);
+    },
+    read: (req,res) => {
+        res.status(200).send(schedule);
+    },
+    update: (req,res) => {
+        const {firstName, lastName, jerseyNumber, height, position} = req.body;
+        const updateID = req.params.id;
+        const playerIndex = roster.findIndex(player => player.id == updateID);
+        let player = roster[playerIndex];
+
+        roster[playerIndex] = {
+            id: updateID,
+            firstName: firstName || roster.firstName,
+            lastName: lastName || roster.lastName,
+            jerseyNumber: jerseyNumber || roster.jerseyNumber,
+            height: height || roster.height,
+            position: position || roster.position,
+
+        };
+
+        res.status(200).send(roster);
+    },
+    delete: (req,res) => {
+        const deleteID = req.params.id;
+        playerIndex = roster.findIndex(player => player.id == deleteID);
+        roster.splice(deleteID,1);
+        res.status(200).send(roster);
+    }
+    
+}
